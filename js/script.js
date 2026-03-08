70e1b2396e9d908a20d625b032831638afa72c86
ajmainhossain7 <ajmainh0@gmail.com>
2026-03-08 12:56:56 +0600

    Login page complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("hello login")
+
+// login from section
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+    event.preventDefault()
+    const user = document.getElementById("username").value
+    const pass = document.getElementById("password").value
+    if (user === "admin" && pass === "admin123") {
+        localStorage.setItem("auth", "true")
+        window.location.href = "./dashboard.html"
+    } else {
+        alert("Invalid credentials")
+    }
+})
